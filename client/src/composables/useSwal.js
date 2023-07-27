90c18c5c83ca8b7e64348a5870bacc3f91333a92
Mark Josmil <markjosmilw@gmail.com>
2023-07-27 14:44:31 +0800

    v6

--- client/src/composables/useSwal.js
+++
@@ -1,9 +0,0 @@
-import swal from "sweetalert";
-
-export default function useSwal(text, err) {
-  return swal({
-    title: err ? err : "Success",
-    text: text || "Some error happened",
-    icon: err ? "error" : "success",
-  });
-}
