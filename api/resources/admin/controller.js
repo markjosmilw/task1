8ea8dcf90ed2884e360451e747a931229d106294
Mark Josmil <markjosmilw@gmail.com>
2023-08-10 10:08:43 +0800

    c

--- api/resources/admin/controller.js
+++ api/resources/admin/controller.js
@@ -1,22 +1,36 @@
 const { fetchUsersByPage, getTotalRows } = require("./service");
 
 const getProfileList = async (ctx) => {
-  const { page } = ctx.query;
+  const { search, page } = ctx.query;
+  if (!search) {
+    try {
+      const users = await fetchUsersByPage(page);
+      const totalRows = await getTotalRows();
+      const pageCount = (totalRows.count - (totalRows.count % 10)) / 10;
+      ctx.body = { response: users, pageCount: pageCount };
+    } catch (error) {
+      console.log(error);
+      ctx.status = 500;
+      ctx.body = { error: error };
+    }
+    return;
+  }
   try {
-    const users = await fetchUsersByPage(page);
-    const totalRows = await getTotalRows();
-    const pageCount = (totalRows.count - (totalRows.count % 10)) / 10;
-    ctx.body = { response: users, pageCount: pageCount };
+    ctx.body = "search";
   } catch (error) {
     console.log(error);
-    ctx.status = 500;
-    ctx.body = { error: error };
   }
 };
 
+// const getSearchedProfileList = async (ctx) => {
+//   const {search, page} = ctx.query;
+//   console.log(search);
+//   console.log(page);
+// }
+
 module.exports = {
   getProfileList,
+  // getSearchedProfileList
 };
-
 
 //continue admin
